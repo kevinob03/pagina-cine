3d2f77377229d65789f7c5e03062d1e1c8208e94
kevinob03 <kevinob046@gmail.com>
2026-08-05 22:11:30 -0600

    feat: save theme preference

--- src/js/cine.js
+++ src/js/cine.js
@@ -1,13 +1,22 @@
 const botonTema = document.getElementById("tema");
 
+// Revisar si había un tema guardado
+if (localStorage.getItem("tema") === "oscuro") {
+    document.body.classList.add("dark");
+    botonTema.textContent = "☀️ Modo Claro";
+}
+
+// Cambiar tema
 botonTema.addEventListener("click", () => {
 
     document.body.classList.toggle("dark");
 
-    if(document.body.classList.contains("dark")){
+    if (document.body.classList.contains("dark")) {
         botonTema.textContent = "☀️ Modo Claro";
-    }else{
+        localStorage.setItem("tema", "oscuro");
+    } else {
         botonTema.textContent = "🌙 Modo Oscuro";
+        localStorage.setItem("tema", "claro");
     }
 
 });
